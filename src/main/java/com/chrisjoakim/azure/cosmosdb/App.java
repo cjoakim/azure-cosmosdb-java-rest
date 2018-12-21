2dbf4d1b376606c0f14decf6d392496e6fb6d934
Chris Joakim <christopher.joakim@gmail.com>
2018-12-21 10:02:08 -0500

    Initial files created with mvn archetype:generate

---
+++ src/main/java/com/chrisjoakim/azure/cosmosdb/App.java
@@ -0,0 +1,13 @@
+package com.chrisjoakim.azure.cosmosdb;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
